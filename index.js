e4bbc08f6b858ccd2303107fa99c16854bb8d3c1
Aaron <morrisai@gmail.com>
2015-04-17 22:42:42 -0400

    updated readme.md

--- index.js
+++ index.js
@@ -2,17 +2,12 @@
 module.exports = {   
 
 
-  login: function (clientId,apiKey,scopes, callback) {
+  signin: function (clientId,apiKey,scopes, callback) {
 
      require( 'google-client-api' )().then( function( gapi ) {
-
-
-
       
-      
-        // Step 2: Reference the API key
-        gapi.client.setApiKey(apiKey);    
-        setTimeout(checkAuth,1);
+       gapi.client.setApiKey(apiKey);    
+       setTimeout(checkAuth,1);
       
 
       function checkAuth() {
@@ -26,12 +21,6 @@ module.exports = {
           login();
         }
       }
-
-
-      
-
-
-
         function login(event) {
           // Step 3: get authorization to use private data
           gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
@@ -41,7 +30,7 @@ module.exports = {
   },
 
 
-  logout: function(){
+  signout: function(){
 
     require( 'google-client-api' )().then( function( gapi ) {
 
--- readme.md
+++ readme.md
@@ -9,14 +9,24 @@
 This module provides a simple API for Ouath via the [Google Client API](https://developers.google.com/api-client-library/javascript/dev/dev_jscript).
 
 ## Usage
-```
+###Signin
+```	
+
+		// Get API Credentials from [Google's API Console](https://console.developers.google.com/project)
         var clientId = '*****************';
         var apiKey = '*************';
         var scopes = 'https://www.googleapis.com/auth/analytics';
-		GoogleApi.login(clientId,apiKey,scopes, onLoginCallback);
+		require('google-oauth-api').signin(clientId,apiKey,scopes, onLoginCallback);
 
  ```
 
+ ### Signout
+```	
+
+		
+		require('google-oauth-api').signout();
+
+ ```
 ## License
 
-MIT, see [LICENSE.md](http://github.com/Jam3/threejs-uv-projection-factory/blob/master/LICENSE.md) for details.
+MIT, see [LICENSE.md](https://github.com/Morrisai/Google-Oauth-API/blob/master/LICENSE.md) for details.
